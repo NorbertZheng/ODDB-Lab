e21d6cdf657a005d321a79b51045195f71dc4a10
Fassial <2508440340@qq.com>
2020-01-08 01:28:33 +0800

    tools: modify test_writeFile in fileToolset

--- tools/fileToolset/test/test_writeFile/test_writeFile.java
+++ tools/fileToolset/test/test_writeFile/test_writeFile.java
@@ -3,10 +3,14 @@
 public class test_writeFile {
 
 	private static String filePath = "./test/test_writeFile.txt";
-	private static String fileContent = "Hello World!";
+	private static String fileContent1 = "Hello World!", fileContent2 = "123";
 
 	public static void main(String[] args) {
-		boolean t_writeFile = fileToolset.writeFile(test_writeFile.filePath, test_writeFile.fileContent);
+		boolean t_writeFile = fileToolset.writeFile(test_writeFile.filePath, test_writeFile.fileContent1);
+
+		System.out.println("test fileToolset.writeFile, flag: " + t_writeFile);
+
+		t_writeFile = fileToolset.writeFile(test_writeFile.filePath, test_writeFile.fileContent2);
 
 		System.out.println("test fileToolset.writeFile, flag: " + t_writeFile);
 	}
