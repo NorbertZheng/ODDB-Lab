7a3ee898e3813b551b4764130e3f08c0c8ba3081
Fassial <2508440340@qq.com>
2020-01-28 11:13:50 +0800

    kernel: support SELECT_TUPLE in SQLParser

--- kernel/SQLParser/test/test_SQLParser/test_SQLParser.java
+++ kernel/SQLParser/test/test_SQLParser/test_SQLParser.java
@@ -1,6 +1,25 @@
 public class test_SQLParser {
+	// CREATE_CLASS
 	// final static String expr = "CREATE CLASS product ( id int, name char , price int ); ";
-	final static String expr = "DROP CLASS usproduct;";
+	// DROP_CLASS
+	// final static String expr = "DROP CLASS usproduct;";
+	// final static String expr = "DROP CLASS product;";
+	// SELECT_TUPLE
+	// final static String expr = "SELECT id , name , price FROM product;";
+	// final static String expr = "SELECT id , name , price FROM product;";
+	// final static String expr = "SELECT name AS name, (price/7) AS usprice FROM product WHERE price>5000;";
+	// final static String expr = "SELECT sales , name , usprice FROM usproduct;";
+	// final static String expr = "SELECT sales , name , usprice FROM usproduct;";
+	// final static String expr = "SELECT id AS id, (price*15) AS jpprice FROM product WHERE price<=7000;";
+	// final static String expr = "SELECT id, jpprice FROM jpproduct;";
+	// final static String expr = "SELECT sales,name,usprice FROM usproduct;";
+	// final static String expr = "SELECT id, jpprice FROM jpproduct;";
+	// final static String expr = "SELECT sales,name,usprice FROM usproduct;";
+	// final static String expr = "SELECT id, jpprice FROM jpproduct;";
+	// final static String expr = "SELECT sales,name,usprice FROM usproduct;";
+	// final static String expr = "SELECT id, jpprice FROM jpproduct;";
+	// final static String expr = "SELECT sales,name,usprice FROM usproduct;";
+	final static String expr = "SELECT id, jpprice FROM jpproduct;";
 
 	public static void main(String[] args) {
 		try {
